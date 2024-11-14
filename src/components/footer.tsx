b6b13db4e2f532682e30546fa6c4e10bcbf6dd13
adityadewhy-local <adityadewhy@gmail.com>
2024-11-14 15:11:45 +0530

    made footer functional

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Footer() {
 	return (
-		<div className="m-2 bg-gray-900 border-t-2 py-4 pr-2 flex items-center justify-end text-2xl ">
+		<a
+			href="https://github.com/adityadewhy/goodWatch"
+			target="_blank"
+			className="m-2 bg-gray-900 border-t-2 py-4 pr-2 flex items-center justify-end text-2xl underline"
+		>
 			source code available on github
-		</div>
+		</a>
 	);
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import SearchTitles from "./searchTitles";
 import SearchUsers from "./searchUsers";
 import AvatarPopover from "./avatarPopover";
+import Link from "next/link";
 
 interface NavbarProps {
 	onSearch: (term: string) => void;
@@ -10,14 +11,15 @@ interface NavbarProps {
 export default function Navbar({onSearch}: NavbarProps) {
 	return (
 		<div className="m-2 bg-gray-900 border-b-2 py-4 flex items-center justify-around text-2xl text-white">
-			<div className="flex items-center">
+			
+			<Link className="flex items-center" href="/">
 				<img
 					src="https://flowbite.com/docs/images/logo.svg"
 					className="h-8"
 					alt="Goodwatch Logo"
 				/>
 				<span className="text-2xl px-2 font-extrabold">GoodWatch</span>
-			</div>
+			</Link>
 
 			<div>
 				<strong>Watchlist</strong>
