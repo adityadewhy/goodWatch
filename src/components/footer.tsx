8f4b1ece2f5b9cdd6560a8a4da225904a5214dab
adityadewhy-local <adityadewhy@gmail.com>
2024-11-15 18:28:25 +0530

    added a inside div

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,12 +2,16 @@ import React from "react";
 
 export default function Footer() {
 	return (
-		<a
+		<div>
+			<a
 			href="https://github.com/adityadewhy/goodWatch"
 			target="_blank"
 			className="m-2 bg-gray-900 border-t-2 py-4 pr-2 flex items-center justify-end text-2xl underline"
 		>
 			source code available on github
 		</a>
+
+		</div>
+		
 	);
 }
