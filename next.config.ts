b1580a454339d6fe0e733eb5c36a00f526484622
adityadewhy-local <adityadewhy@gmail.com>
2024-11-15 16:22:30 +0530

    added image accessing permission for next to load outside images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // Keep existing settings
+  images: {
+    domains: ['m.media-amazon.com'], // Add external domain for images
+  },
 };
 
 export default nextConfig;
